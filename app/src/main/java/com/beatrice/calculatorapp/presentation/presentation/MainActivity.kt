93ea146cc69a28db9d540256394cd5a551c3c486
Beatrice Kinya <gitongakinya1@gmail.com>
2022-12-07 16:48:31 +0300

    initial commit

---
+++ app/src/main/java/com/beatrice/calculatorapp/presentation/presentation/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.beatrice.calculatorapp.presentation.presentation
+
+import android.os.Bundle
+import android.view.Gravity
+import android.widget.LinearLayout
+import android.widget.LinearLayout.LayoutParams
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.setMargins
+import com.beatrice.calculatorapp.R
+import com.beatrice.calculatorapp.presentation.presentation.components.CalculatorLabelTv
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val parentLayout = LinearLayout(this).apply {
+            val parentParams = LayoutParams(
+                LayoutParams.MATCH_PARENT,
+                LayoutParams.MATCH_PARENT
+            )
+            parentParams.setMargins(48)
+            gravity = Gravity.CENTER
+            orientation = LinearLayout.VERTICAL
+            layoutParams = parentParams
+            val bgColor = ContextCompat.getColor(context, R.color.white)
+            setBackgroundColor(bgColor)
+        }
+
+
+        val num1Label = CalculatorLabelTv(this, "First Number")
+        val num2Label = CalculatorLabelTv(this, "SecondNumber")
+        parentLayout.addView(num1Label)
+        parentLayout.addView(num2Label)
+        setContentView(parentLayout)
+    }
+}
---
+++ app/src/main/java/com/beatrice/calculatorapp/presentation/presentation/components/CalculatorLabelTv.kt
@@ -0,0 +1,19 @@
+package com.beatrice.calculatorapp.presentation.presentation.components
+
+import android.content.Context
+import android.widget.LinearLayout.LayoutParams
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.beatrice.calculatorapp.R
+
+fun CalculatorLabelTv(context: Context, label: String): TextView = TextView(context).apply {
+    val params = LayoutParams(
+        LayoutParams.MATCH_PARENT,
+        LayoutParams.WRAP_CONTENT
+    )
+    params.setMargins(0,16, 0, 16)
+    layoutParams = params
+    text = label
+    textSize = 17F
+    setTextColor(ContextCompat.getColor(context, R.color.blue_gray_900))
+}
